23f1cc5fb3e016eeab873607b1ef5fc74ea6fc6e
Nims972 <nirmalypatel@gmail.com>
2018-07-04 09:12:31 +0530

    added some shared features

--- app/src/main/java/com/ala/android/ala/MainActivity.java
+++ app/src/main/java/com/ala/android/ala/MainActivity.java
@@ -1,6 +1,8 @@
 package com.ala.android.ala;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,16 +15,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    SharedPreferences sharedpreferences;
+    public static final String MyPREFERENCES = "GroupPrefs" ;
+    public static final String Name1 = "name1Key";
+    public static final String Name2 = "name2Key";
+    public static final String Name3 = "name3Key";
+    public static final String En1 = "en1Key";
+    public static final String En2 = "en2Key";
+    public static final String En3 = "en3Key";
+    public static final String GroupID = "groupKey";
+
+    TextView groupid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        groupid=(TextView)findViewById(R.id.groupid);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+       sharedpreferences = getSharedPreferences(MyPREFERENCES, MODE_PRIVATE);
+       if(sharedpreferences.contains(GroupID))
+            groupid.setText(sharedpreferences.getString(GroupID,""));
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/ala/android/ala/RegActivity.java
+++ app/src/main/java/com/ala/android/ala/RegActivity.java
@@ -1,15 +1,17 @@
 package com.ala.android.ala;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegActivity extends AppCompatActivity {
-    EditText name1,name2,name3;
+    EditText name1,name2,name3,en1,en2,en3,mail,batch;
     Button reg;
     SharedPreferences sharedpreferences;
 
@@ -17,6 +19,9 @@ public class RegActivity extends AppCompatActivity {
     public static final String Name1 = "name1Key";
     public static final String Name2 = "name2Key";
     public static final String Name3 = "name3Key";
+    public static final String En1 = "en1Key";
+    public static final String En2 = "en2Key";
+    public static final String En3 = "en3Key";
     public static final String GroupID = "groupKey";
 
 
@@ -28,6 +33,11 @@ public class RegActivity extends AppCompatActivity {
         name1=(EditText)findViewById(R.id.name1);
         name2=(EditText)findViewById(R.id.name2);
         name3=(EditText)findViewById(R.id.name3);
+        en1=(EditText)findViewById(R.id.en1);
+        en2=(EditText)findViewById(R.id.en2);
+        en3=(EditText)findViewById(R.id.en3);
+        mail=(EditText)findViewById(R.id.email);
+        batch=(EditText)findViewById(R.id.batch);
         reg=(Button)findViewById(R.id.regbutton);
 
         sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
@@ -39,8 +49,26 @@ public class RegActivity extends AppCompatActivity {
                 data.putString(Name1,name1.getText().toString());
                 data.putString(Name2,name2.getText().toString());
                 data.putString(Name3,name3.getText().toString());
-                data.putString(GroupID,"Group 1");
-                data.commit();
+                data.putString(En1,en1.getText().toString());
+                data.putString(En2,en2.getText().toString());
+                data.putString(En3,en3.getText().toString());
+
+                String s1,s2,s3;
+                s1=en1.getText().toString();
+                s2=en2.getText().toString();
+                s3=en3.getText().toString();
+
+                if(s1.length()!=12 || s2.length() !=12 || s3.length()!=12)
+                {
+                    Toast.makeText(getApplicationContext(),"Enter valid Enrollment no.",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    String id=s1.substring(10)+s2.substring(10)+s3.substring(10);
+                    data.putString(GroupID, id);
+                    data.commit();
+                    Intent up=new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(up);
+                }
             }
         });
 
